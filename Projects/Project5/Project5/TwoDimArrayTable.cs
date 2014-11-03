79d81ded5a806af9f279fff8350d25c47c059ad9
Twenty5Clocks <JTDriesler@gmail.com>
2014-11-03 10:48:50 -0800

    adding proj5

---
+++ Projects/Project5/Project5/TwoDimArrayTable.cs
@@ -0,0 +1,39 @@
+﻿/**********************place in your source code as first few lines**************************
+* Name: Jeremy Driesler
+* Class: CPMS 378,
+* Assignment #: 5, Two Dimension Array 
+* Date Due: 11-04-14
+* Problem Description (Explains the purpose and the design of your program. It can include
+     discussion the method used to solve the problem.) 
+ * To find the minimum, maximum, and the sum of
+*  three integers input by the user. The user is asked to enter three
+*  integers. The maximum, the minimum, and the sum of the numbers are
+*  calculated. The results are displayed to the user.
+*  Type	Variable	Description	
+*Input:
+        int num1,       // declare first number
+            num2,       // declare second number
+            num3,       // declare third number			
+*Output:
+int 	sum,        // declare sum of numbers
+            average,    // declare average of numbers
+            product,    // declare product of numbers
+            smallest,   // declare smallest of numbers
+            largest;    // declare largest of numbers
+******************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project5
+{
+    class TwoDimArrayTable
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
